e6fe2d5fadb99584ca1eaa663581ba306801a3cb
icza <iczaaa@gmail.com>
2016-06-15 05:22:05 +0200

    Added protocol for 3.3.2 reps. New release (6.2.7).

--- src-app/hu/scelight/sc2/rep/s2prot/Protocol.java
+++ src-app/hu/scelight/sc2/rep/s2prot/Protocol.java
@@ -75,7 +75,7 @@ public class Protocol {
 	 * This protocol handler is used to decode the replay header. Recommended to use the protocol handler for the latest supported base build because it's the
 	 * most likely needed).
 	 */
-	public static final Protocol DEFAULT = get( 42932 );
+	public static final Protocol DEFAULT = get( 43478 );
 	
 	
 	
